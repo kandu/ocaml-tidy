4b8997ae02f39059cab04fe6c94e01fcd6fd6d63
ZAN DoYe <zandoye@gmail.com>
2015-07-13 21:06:37 +0800

    getValue

--- stub.c
+++ stub.c
@@ -274,6 +274,21 @@ CAMLprim value tidyNodeGetType_stub(value node) {
     CAMLreturn(result);
 }
 
+CAMLprim value tidyNodeGetValue_stub(value doc, value node) {
+    CAMLparam2(doc, node);
+    CAMLlocal1(result);
+    TidyBuffer buffer;
+    tidyBufInit(&buffer);
+    if (tidyNodeGetValue(doc_val(doc), node_val(node), &buffer)) {
+        result= caml_alloc(1, 0);
+        Store_field(result, 0, caml_copy_string((char*)buffer.bp));
+    } else {
+        result= Val_int(0); // None
+    }
+    tidyBufFree(&buffer);
+    CAMLreturn(result);
+}
+
 CAMLprim value tidyNodeGetName_stub(value node) {
     CAMLparam1(node);
     CAMLlocal1(result);
